c4281d8c5429b39eca61a37e5813709a75804c86
Christian Miller <cnmiller127@gmail.com>
2020-08-14 14:05:48 -0500

    logo

--- src/components/Jumbotron.js
+++ src/components/Jumbotron.js
@@ -4,7 +4,7 @@ function Jumbotron(){
 return(
     <div className="jumbotron jumbotron-fluid bg-info m-0 text-center">
   <div className="container">
-    <h1 className ="display-4">Heptahedron Corp <img className = "hept" src = "/images/hept.jpg"></img></h1>
+    <h1 className ="display-4">Heptahedron Corp <img className = "hept" src = "https://cnmiller127.github.io/Employee-Directory/images/hept.jpg" alt="logo"></img></h1>
     <h2 className="subheader">Employee Directory</h2>
   </div>
 </div>
